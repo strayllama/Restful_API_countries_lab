b48b02f7196f176f656eac4219871459af3e5617
Paul Clatworthy <strayllama@gmail.com>
2018-04-18 11:33:41 +0100

    Global local array of countries created. Drop down populated.

--- public/app.js
+++ public/app.js
@@ -1,14 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const getCountries = document.querySelector('#getCountries');
-  getCountries.addEventListener('click', handleGetCountries);
+const apiCountryList = [];
 
+document.addEventListener('DOMContentLoaded', () => {
+  // const getCountries = document.querySelector('#getCountries');
+  // getCountries.addEventListener('click', handleGetCountries);
+  const url = 'https://restcountries.eu/rest/v2/all';
+  makeRequest(url, requestComplete);
 });
 
 // and use this handle with the
-const handleGetCountries = function () {
-  const url = 'https://restcountries.eu/rest/v2/all';
-  makeRequest(url, requestComplete);
-};
+// const handleGetCountries = function () {
+//   const url = 'https://restcountries.eu/rest/v2/all';
+//   makeRequest(url, requestComplete);
+// };
 
 // The steps to get info from any API
 const makeRequest = function (url, callback) {
@@ -23,18 +26,21 @@ const requestComplete = function () {
   if (this.status !==200) return; // this. is the request object. should be 200, else an error object
   const jsonString = this.responseText;  // take response
   const countries = JSON.parse(jsonString); // make to useable javascript object
-  populateList(countries); // use array with our function to create countries
-  console.dir(countries);
+  countries.forEach((country) => {
+    apiCountryList.push(country);
+  });
+  console.dir(apiCountryList);
+  populateDropDown();
 };
 
 
-const populateList = function (countries) {
-  const ul = document.querySelector('#country-list');
-
-  countries.forEach((country) => {
-    const li = document.createElement('li');
-
-    li.textContent = country.name + "  Population: " + country.population;
-    ul.appendChild(li);
+const populateDropDown = function () {
+  const selectDropDown = document.querySelector('#country-picker');
+   apiCountryList.forEach((country) => {
+   const optionCountry = document.createElement('option');
+   optionCountry.value = country.coic;
+   optionCountry.textContent = country.name;
+   selectDropDown.appendChild(optionCountry);
   });
+
 };
